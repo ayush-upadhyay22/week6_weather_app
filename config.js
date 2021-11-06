81a8f0836a33476ab4b48d3129798810111c00ec
ayush-upadhyay22 <ayush.upadhyay220302@gmail.com>
2021-11-06 15:11:22 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "dd1660ea24a9e7b9fa8d2761f82fa142"
+    }
+}
+
+module.exports = constants;
